efe18f43bcff229af4b4946700f7175119271424
Eugene Fedorov <vadimology@yandex.ru>
2018-12-08 21:53:51 +0300

    [Component] Add changing balance

--- app/screens/settings/changeBalance.js
+++ app/screens/settings/changeBalance.js
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Input from '../../components/shared/input';
+import Button from '../../components/shared/button';
 import { connect } from 'react-redux';
 import { replenishBalance, withdrawBalance } from '../../actions/currentUser';
 import { currentUserIdSelector } from '../../selectors/currentUserSelectors';
+import MultiSwitch from '../../components/shared/multi_switch/multiSwitch';
 
 class ChangeBalanceScreen extends Component {
+  state = {
+    moneyAmount: '',
+  }
+
+
+  onChangeText = (key, value) => {
+    this.setState({
+      [key]: value,
+    });
+  }
+
+  replenishBalance() {
+    const { replenishBalance } = this.props;
+    const {
+      moneyAmount,
+    } = this.state;
+    replenishBalance(
+      this.props.userId,
+      parseInt(moneyAmount),
+    );
+  }
+
+  withdrawBalance() {
+    const { withdrawBalance } = this.props;
+    const {
+      moneyAmount,
+    } = this.state;
+    withdrawBalance(
+      this.props.userId,
+      parseInt(moneyAmount),
+    );
+  }
+
 
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Balance screen</Text>
+        <Input
+          placeholder="Money"
+          type="moneyAmount"
+          keyboardType="numeric"
+          onChangeText={this.onChangeText}
+          value={this.state.moneyAmount}
+        />
+        <Button
+          title="Replenish Balance"
+          onPress={this.replenishBalance.bind(this)}
+        />
+        <Button
+          title="Withdraw Balance"
+          onPress={this.withdrawBalance.bind(this)}
+        />
       </View>
     );
   }
