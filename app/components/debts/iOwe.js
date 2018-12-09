45fdc2d6c0c208988cdb855a8088433fbce4cc84
Eugene Fedorov <vadimology@yandex.ru>
2018-12-09 21:03:07 +0300

    [Fix bugs] Fix styles

--- app/components/debts/iOwe.js
+++ app/components/debts/iOwe.js
@@ -5,27 +5,18 @@ import moment from 'moment'
 
 const IOweDebt = ({created_at, id, credit_percentage, current_size, repayDebt}) => {
   return (
-    // <View>
-    //   <Text>created_at: { created_at } || current_size: {current_size} || credit_percentage: {credit_percentage} || ID: {id}</Text>
-    //     <Button
-    //       title="Repay debt"
-    //       onPress={() => {
-    //         repayDebt(id);
-    //       }}
-    //     />
-    // </View>
     <View>
-    <PricingCard
-      color='#4f9deb'
-      title='Debt'
-      price={`$ ${current_size}`}
-      info={[ `${moment(created_at).format('MMMM Do YYYY, h:mm:ss a')}`, `${credit_percentage} percentage`]}
-      button={{ title: 'Repay Debt', icon: 'fingerprint' }}
-      onButtonPress={() => {
-            repayDebt(id);
-          }}
-    />
-</View>
+      <PricingCard
+        color='#4f9deb'
+        title='Debt'
+        price={`$ ${current_size}`}
+        info={[ `${moment(created_at).format('MMMM Do YYYY, h:mm:ss a')}`, `${credit_percentage} percentage`]}
+        button={{ title: 'Repay Debt', icon: 'fingerprint' }}
+        onButtonPress={() => {
+              repayDebt(id);
+            }}
+      />
+    </View>
   );
 };
 
